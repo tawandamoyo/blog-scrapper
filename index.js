a6ed1741fca7b9faf5a19022b16ca4010744756a
Tawanda Moyo <moyotawanda@gmail.com>
2022-08-23 18:02:21 +0200

    add eslint

--- index.js
+++ index.js
@@ -1,76 +1,73 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs');
 
-let URL = "https://bigsr.africa/author/dr-alex-magaisa";
+const URL = 'https://bigsr.africa/author/dr-alex-magaisa';
 (async () => {
-    const browser = await puppeteer.launch({
-        headless: true
+  const browser = await puppeteer.launch({
+    headless: true,
+  });
+  const page = await browser.newPage();
+  await page.goto(URL);
+
+  const pagesToScrape = 44;
+  let currentPage = 1;
+  let postLinks = [];
+
+  while (currentPage <= pagesToScrape) {
+    const postLinksOnPage = await page.evaluate(() => {
+      const links = [];
+      const postDetails = document.querySelectorAll('h3 a');
+      postDetails.forEach((post) => {
+        links.push(post.href);
+      });
+      return links;
     });
-    const page = await browser.newPage();
-    await page.goto(URL);
-
-    let pagesToScrape = 44;
-    let currentPage = 1;
-    let postLinks = [];
-
-    while (currentPage <= pagesToScrape) {
-        let postLinksOnPage = await page.evaluate(() => {
-            let links = [];
-            let postDetails = document.querySelectorAll("h3 a");
-            postDetails.forEach((post) => {
-                links.push(post.href);
-            });
-            return links;
-        });
-        postLinks = postLinks.concat(postLinksOnPage);
-        if (currentPage < pagesToScrape) {
-            await page.goto(`${URL}/page/${currentPage + 1}`);
-            page.waitForNetworkIdle;
-        }
-        currentPage++;
+    postLinks = postLinks.concat(postLinksOnPage);
+    if (currentPage < pagesToScrape) {
+      await page.goto(`${URL}/page/${currentPage + 1}`);
+      page.waitForNetworkIdle;
     }
-    await page.close();
+    currentPage += 1;
+  }
+  await page.close();
 
-    postLinks = [...new Set(postLinks)];
-    console.log(postLinks.length);
+  postLinks = [...new Set(postLinks)];
 
-    for (let link of postLinks) {
-        try {
-            const page = await browser.newPage();
-            await page.goto(link, {
-                waitUntil: 'networkidle2'
-            });
+  for (const link of postLinks) {
+    try {
+      const page = await browser.newPage();
+      await page.goto(link, {
+        waitUntil: 'networkidle2',
+      });
 
-            let postData = await page.evaluate(() => {
-                const postTitle = document.querySelector("h1").innerText;
-                const postDate = document.querySelector(".td-post-date").innerText;
+      const postData = await page.evaluate(() => {
+        const postTitle = document.querySelector('h1').innerText;
+        const postDate = document.querySelector('.td-post-date').innerText;
 
-                const dateDetails = postDate.split(" ");
-                const month = dateDetails[0];
-                const year = dateDetails[2];
+        const dateDetails = postDate.split(' ');
+        const month = dateDetails[0];
+        const year = dateDetails[2];
 
-                return {
-                    title: postTitle,
-                    month: month,
-                    year: year
-                }
+        return {
+          title: postTitle,
+          month,
+          year,
+        };
+      });
 
-            });
+      const path = `bsr/${postData.year}/${postData.month}`;
 
-            const path = `bsr/${postData.year}/${postData.month}`;
+      fs.mkdir(path, { recursive: true }, (error) => {
+        if (error) throw error;
+      });
 
-            fs.mkdir(path, { recursive: true }, (error) => {
-                if (error) throw error;
-            });
+      await page.pdf({
+        path: `bsr/${postData.year}/${postData.month}/${postData.title}.pdf`,
+      });
 
-            await page.pdf({
-                path: `bsr/${postData.year}/${postData.month}/${postData.title}.pdf`
-            });
-
-            await page.close();
-
-        } catch (err) {
-            console.error(err)
-        }
+      await page.close();
+    } catch (err) {
+      console.error(err);
     }
+  }
 })();
